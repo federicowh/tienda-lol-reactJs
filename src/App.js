d6db258e8230e81b78479a99ad3db76755be4ba5
Federico Weimann <federicoweimannh@gmail.com>
2021-09-24 20:54:45 -0300

    Agregamos estilos al Proyecto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'boxicons';
 
 function App() {
   return (
     <div>
       <header id="encabezado">
-        <h1 class="encabezado-tituloPrincipal">Hola Mundo.</h1>
+        <h1 class="encabezado-tituloPrincipal">¡Hola! Bienvenido a la Tienda. <box-icon name='happy'></box-icon></h1>
 
-        <p class="encabezado-parrafoPrincipal">Bienvenidos a todos y todas.</p>
+        <p class="encabezado-parrafoPrincipal">Aquí podrás encontrar productos del videojuego League of Legends. <box-icon name='like' ></box-icon></p>
       </header>
+
+      <main id="contenido">
+        <p class="contenido-parrafoPrincipal">Aquí irán los productos</p>
+      </main>
+
+      <footer id="pie">
+          <p class="pie-parrafoPrincipal">Pie de Página</p>
+      </footer>
     </div>
   );
 }
