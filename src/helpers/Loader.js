0110d7cdd87f5d02cbc2cefe7d1526026f4b00a1
Federico Weimann <federicoweimannh@gmail.com>
2021-10-09 12:10:29 -0300

    Cambiamos class a className para evitar errores de consola

--- src/helpers/Loader.js
+++ src/helpers/Loader.js
@@ -4,25 +4,27 @@ import '../helpers/loader.css';
 export const Loader = () => {
     return (
         <>
-            <div class="loader">
-                <div class="loader-inner">
-                    <div class="loader-line-wrap">
-                        <div class="loader-line"></div>
+
+            <div className="loader">
+                <div className="loader-inner">
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
                     </div>
-                    <div class="loader-line-wrap">
-                        <div class="loader-line"></div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
                     </div>
-                    <div class="loader-line-wrap">
-                        <div class="loader-line"></div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
                     </div>
-                    <div class="loader-line-wrap">
-                        <div class="loader-line"></div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
                     </div>
-                    <div class="loader-line-wrap">
-                        <div class="loader-line"></div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
                     </div>
                 </div>
             </div>
+
         </>
     )
 }
