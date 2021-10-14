e059d817ab37f8c23acef42172489782acf75e6a
Federico Weimann <federicoweimannh@gmail.com>
2021-10-14 01:53:23 -0300

    Actualizamos la base de datos

--- src/data/stock.js
+++ src/data/stock.js
@@ -4,55 +4,63 @@ export const stock = [
         name: "Evelynn Unlocked",
         description: "Evelynn se convierte en la #12 de la serie Unlocked.",
         price: "11.500,00",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt7e10d0aacd337719/5f59440a572b123c0e093243/12-09-2019171309454_Riot_20Games_01.png?auto=webp&width=1903&quality=85"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt7e10d0aacd337719/5f59440a572b123c0e093243/12-09-2019171309454_Riot_20Games_01.png?auto=webp&width=1903&quality=85",
+        category: 'figuras1'
     },
     {
         id: 2,
         name: "Figura de Ahri Unlocked",
         description: "Ahri es una vastaya conectada de forma innata al poder latente de Runaterra y es capaz de convertir la magia en orbes de energía pura. Le gusta jugar con su presa, manipulando sus emociones antes de devorar su esencia de vida. A pesar de su naturaleza depredadora, Ahri posee cierto sentido de la empatía, ya que recibe recuerdos de cada alma que consume.",
         price: "14.500,00",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt2ccb7909760c15c1/5fc57b19fd99385ff6009fae/Ahri_00.png"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt2ccb7909760c15c1/5fc57b19fd99385ff6009fae/Ahri_00.png",
+        category: 'figuras1'
     },
     {
         id: 3,
         name: "Figura de Jinx Unlocked",
         description: "¡Jinx! ¡De Jinx... a secas! Bah. No importa para nada en qué lugar de la Grieta te encuentres, Jinx saldrá a por ti, y más ahora que es la número 7 de la línea de figuras de Unlocked.",
         price: "19.999,99",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/bltf906a80c975e890b/5f594267fa91f32fa9f6996c/12-03-2018_2022_10_57_240_JINX-FRONT.png"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/bltf906a80c975e890b/5f594267fa91f32fa9f6996c/12-03-2018_2022_10_57_240_JINX-FRONT.png",
+        category: 'figuras1'
     },
     {
         id: 4,
         name: "Figura de Viego Unlocked",
         description: "Viego, soberano de un reino antaño desaparecido, murió hace mil años cuando su intento por devolverle la vida a su esposa desencadenó una catástrofe mágica conocida como la Ruina. Ahora, Viego, trasformado en un poderoso espectro atormentado por el anhelo obsesivo de recuperar a su reina muerta siglos atrás, se alza como el Rey Arruinado, capaz de controlar los letales Harrowing mientras barre Runaterra en busca de algo que pueda traer de vuelta a su amada algún día y destruye todo a su paso con la Niebla Negra que emana incesablemente de su cruel corazón roto.",
         price: "14.500,00",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/bltf356d1ba8a98cd9e/60de2cef102c30485866235c/Riot_Sentinels_Promo-Ecomm-Portrait-Viego_Statue2560x3200.png"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/bltf356d1ba8a98cd9e/60de2cef102c30485866235c/Riot_Sentinels_Promo-Ecomm-Portrait-Viego_Statue2560x3200.png",
+        category: 'figuras1'
     },
     {
         id: 5,
         name: "Figura de Jhin Unlocked",
         description: "Con su pistola como pincel, la obra de Jhin muestra un arte brutal, tanto para las víctimas como para los espectadores. Encuentra un cruel placer en la escenificación atroz, lo cual lo convierte en el candidato ideal para mandar el más poderoso de los mensajes: el terror.",
         price: "19.999,99",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt05c98a139686660f/5fb5a9cc72a3526f28dbd226/Jhin_Wide_Callout.png"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt05c98a139686660f/5fb5a9cc72a3526f28dbd226/Jhin_Wide_Callout.png",
+        category: 'figuras2'
     },
     {
         id: 6,
         name: "Yasuo Unlocked",
         description: "La armonía es una mentira para forzar la obediencia. Una espada, un propósito. El caos acabará con el orden. La edición especial #11 de Yasuo heraldo del caos ya es Unlocked.",
         price: "15.999,99",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt3df729accc83df5f/5f59430bd6449530d09f121b/11-25-2019173656250_Yasuo-Front.png"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt3df729accc83df5f/5f59430bd6449530d09f121b/11-25-2019173656250_Yasuo-Front.png",
+        category: 'figuras2'
     },
     {
         id: 7,
         name: "Riven guerrera Unlocked",
         description: "La armonía someterá a la discordia. El amanecer acabará con la oscuridad. El orden pondrá fin al caos. La edición especial #10 de Riven guerrera del orden ya es Unlocked.",
         price: "18.999,99",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/bltcaa326d4bb8d10a3/5f5943032e429507af9befea/11-25-2019173938556_Riven-Front.png"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/bltcaa326d4bb8d10a3/5f5943032e429507af9befea/11-25-2019173938556_Riven-Front.png",
+        category: 'figuras2'
     },
     {
         id: 8,
         name: "Figura de Ekko Unlocked",
         description: "Consigue lo imposible a la primera, una y otra vez. Ekko ya es Unlocked.",
         price: "13.500,00",
-        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt1bcb58d5fa50cf5e/5f594158d6449530d09f11ab/2018-09-07_2000_15_14.955_riot_games-2017-08-16_star_guardian_ekko_worlds_tibbers_3708_copy.png"
+        img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt1bcb58d5fa50cf5e/5f594158d6449530d09f11ab/2018-09-07_2000_15_14.955_riot_games-2017-08-16_star_guardian_ekko_worlds_tibbers_3708_copy.png",
+        category: 'figuras2'
     }
 ]
