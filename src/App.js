0297a5042cbff6f8279df1f3aa9aabc8566e6a4c
Federico Weimann <federicoweimannh@gmail.com>
2021-10-14 01:52:57 -0300

    Agregamos el Router y lo organizamos lo más prolijo posible

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route,
+  Link,
+  NavLink, } from "react-router-dom";
 import './global.css';
 import 'boxicons';
 import { Navbar } from './components/Navbar/Navbar';
 import { HomeView } from './components/HomeView/HomeView';
+import { ItemDetailContainer } from './components/HomeView/Items/ItemDetailContainer/ItemDetailContainer';
 import { Footer } from './components/Footer/Footer';
 
 function App() {
-
   return (
-    <>
-      <Navbar />
+    <Router>
+      <div>
+        <Navbar />
 
-      <HomeView />
+        <Switch>
+          <Route exact path="/">
+            <HomeView />
+          </Route>
 
-      <Footer />
-    </>
+          <Route exact path="/productos/:categoryId">
+            <HomeView />
+          </Route>
+
+          <Route exact path="/detail/:itemId">
+            <ItemDetailContainer />
+          </Route>
+
+          <Route exact path="/contacto">
+            <h1>Contacto</h1>
+          </Route>
+
+          <Route exact path="/cart">
+            <h1>Carrito</h1>
+          </Route>
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 
 }
