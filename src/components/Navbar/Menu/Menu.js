295fe99c19aadf4a1615e88fa5959b7b53e66169
Federico Weimann <federicoweimannh@gmail.com>
2021-10-25 16:47:44 -0300

    Arreglamos el NavLink del menú que no estaba haciendo nada

--- src/components/Navbar/Menu/Menu.js
+++ src/components/Navbar/Menu/Menu.js
@@ -7,11 +7,11 @@ export const Menu = () => {
     return (
         <>
             <nav className="navbar">
-                <NavLink activeClassName={'activeLink'} exact to="/tienda-lol-reactJs">Inicio</NavLink>
-                <NavLink activeClassName={'activeLink'} exact to="/productos/Figuras">Figuras</NavLink>
-                <NavLink activeClassName={'activeLink'} exact to="/productos/Remeras">Remeras</NavLink>
-                <NavLink activeClassName={'activeLink'} exact to="/productos/Camperas">Camperas</NavLink>
-                <NavLink activeClassName={'activeLink'} exact to="/contacto">Contacto</NavLink>
+                <NavLink activeStyle={{color: "#EBA272", textDecoration: "overline 2px solid #EBA272"}} exact to="/tienda-lol-reactJs">Inicio</NavLink>
+                <NavLink activeStyle={{color: "#EBA272", textDecoration: "overline 2px solid #EBA272"}} exact to="/productos/Figuras">Figuras</NavLink>
+                <NavLink activeStyle={{color: "#EBA272", textDecoration: "overline 2px solid #EBA272"}} exact to="/productos/Remeras">Remeras</NavLink>
+                <NavLink activeStyle={{color: "#EBA272", textDecoration: "overline 2px solid #EBA272"}} exact to="/productos/Camperas">Camperas</NavLink>
+                <NavLink activeStyle={{color: "#EBA272", textDecoration: "overline 2px solid #EBA272"}} exact to="/contacto">Contacto</NavLink>
             </nav>
         </>
     )
